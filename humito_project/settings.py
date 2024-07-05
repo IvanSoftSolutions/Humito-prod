a3badb0c5e2c278a4b10f6f1dae6798a5bcd2fc4
ivand <ivan.daniel.hdz@gmail.com>
2024-07-05 14:00:01 -0600

    Change static files urls

--- humito_project/settings.py
+++ humito_project/settings.py
@@ -121,9 +121,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATICFILES_DIRS = [os.path.join(BASE_DIR, "humito_site/static/humito_site")]
+# STATICFILES_DIRS = [os.path.join(BASE_DIR, "humito_site/static/humito_site")]
 STATIC_URL = "static/"
-STATIC_ROOT = os.path.join(BASE_DIR, "humito_site/staticfiles/humito_site")
+# STATIC_ROOT = os.path.join(BASE_DIR, "humito_site/staticfiles/humito_site")
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
