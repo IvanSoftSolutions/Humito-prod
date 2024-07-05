109fd35c0c43fbde0a9d01eb7c3f3841083a3cd9
ivand <ivan.daniel.hdz@gmail.com>
2024-07-05 13:58:32 -0600

    Rename details.css to detail.css

--- humito_project/settings.py
+++ humito_project/settings.py
@@ -121,9 +121,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATICFILES_DIRS = [os.path.join(BASE_DIR, "humito_site/static")]
+STATICFILES_DIRS = [os.path.join(BASE_DIR, "humito_site/static/humito_site")]
 STATIC_URL = "static/"
-STATIC_ROOT = os.path.join(BASE_DIR, "humito/staticfiles")
+STATIC_ROOT = os.path.join(BASE_DIR, "humito_site/staticfiles/humito_site")
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
